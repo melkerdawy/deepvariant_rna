b408bcf12ee3d069f5d76cd255c8f5ca1d2e63c5
melkerdawy <s-elkerdawy@Zewailcity.edu.eg>
2019-07-25 01:40:53 +0200

    Update model_eval_chr21.sh

--- model_eval_chr21.sh
+++ model_eval_chr21.sh
@@ -17,16 +17,25 @@ cd ${OUTPUT_DIR_TRAINING}
 ls model.ckpt-*.index | cut -f 1-2 -d "." > ${Models_file}
 cd -
 
-while IFS= read -r line
-do
-  singularity -s exec -B /usr/lib/locale/:/usr/lib/locale/ --bind input:${OUTPUT_DIR}/ \
+singularity -s exec -B /usr/lib/locale/:/usr/lib/locale/ --bind input:${OUTPUT_DIR}/ \
 deepvariant.simg \
 /opt/deepvariant/bin/model_eval \
 --dataset_config_pbtxt="${OUTPUT_DIR}/eval_set.dataset_config.pbtxt" \
 --checkpoint_dir="${OUTPUT_DIR_TRAINING}" \
 --keep_checkpoint_every_n_hours=0.05 \
---checkpoint_path="${line}" \
 --batch_size=512 > "${LOG_DIR}/eval.log" 2>&1
+
+OUTPUT_DIR_TRAINING_NEW="${PWD}/output_chr1_"$exp/training_output/new
+mkdir -p "${OUTPUT_DIR_TRAINING_NEW}"
+
+
+#head of models.txt
+cp "${line}".* "${OUTPUT_DIR_TRAINING_NEW}"
+#delete the first line
+while IFS= read -r line
+do
+#while grep hatsawy 7aga 
+
 done < ${Models_file}
 
 squeue -l --job ${SLURM_JOB_ID}
